996a4a5c4c199584677f63a307f60685f60606a6
irinszn <ira.sazonova.spb@gmail.com>
2025-03-11 23:38:57 +0300

    fix: mypy

--- src/homeworks/homework_2/kd_tree.py
+++ src/homeworks/homework_2/kd_tree.py
@@ -3,7 +3,7 @@ from typing import Callable, Iterable, Optional
 
 import numpy as np
 from numpy.typing import NDArray
-from scipy.spatial.distance import euclidean  # type: ignore
+from scipy.spatial.distance import euclidean  # type: ignore[no-untyped-def]
 
 from src.homeworks.homework_2.my_heap import MyHeap
 
@@ -70,7 +70,7 @@ class KDTree:
 
         return np.array(neighbours)
 
-    def query_point(self, fixed_point: NDArray, k: int) -> list[tuple[float, ...]]:
+    def query_point(self, fixed_point: NDArray, k: int) -> list[NDArray]:
         heap = MyHeap(k)
 
         def search_rec(node: Optional[Node]) -> None:
--- src/homeworks/homework_2/my_heap.py
+++ src/homeworks/homework_2/my_heap.py
@@ -1,14 +1,15 @@
 import heapq
 
 import numpy as np
+from numpy.typing import NDArray
 
 
 class MyHeap:
-    def __init__(self, k: int):
+    def __init__(self, k: int) -> None:
         self.k = k
         self.heap: list[tuple] = []
 
-    def add_element(self, dist_with_point: tuple):
+    def add_element(self, dist_with_point: tuple) -> None:
         heapq.heappush(self.heap, dist_with_point)
         if len(self.heap) > self.k:
             heapq.heappop(self.heap)
@@ -18,5 +19,5 @@ class MyHeap:
             return -self.heap[0][0]
         return float("inf")
 
-    def get_sorted(self):
+    def get_sorted(self) -> list[NDArray]:
         return [np.array(point) for dist, point in sorted(self.heap, key=lambda t: -t[0])]
